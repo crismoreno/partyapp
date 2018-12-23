98faa1c7e2e0a1049ef48a6a12ce8f4c2a134d6d
Cristina Moreno <moreno.medran@gmail.com>
2018-12-23 17:19:57 +0100

    things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent }
