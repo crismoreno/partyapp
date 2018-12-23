4b684cbb5ac1028aaf405e3c2d2170ee4eab9eb0
Cristina Moreno <moreno.medran@gmail.com>
2018-12-23 14:02:08 +0100

    attempting heroku deploy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('./dist/notcalpi20'));
 
 app.get('/*', (req, res)=>{
-  res.sendFile(path.join(__dirname, '/dist/notcalpi20/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/partyapp/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, ()=>{
