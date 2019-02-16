9e4996cff6f1ffb501bf8643aecdacfb2cd8f160
Cristina Moreno <moreno.medran@gmail.com>
2019-02-16 15:51:02 +0100

    fixing for heroku

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiKey: "AIzaSyAia1TMv4IFsipmXd--tpbnnALbCLAJPow",
+  authDomain: "partyapp-795e5.firebaseapp.com",
+  databaseURL: "https://partyapp-795e5.firebaseio.com",
+  projectId: "partyapp-795e5",
+  storageBucket: "partyapp-795e5.appspot.com",
+  messagingSenderId: "252811103599"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
